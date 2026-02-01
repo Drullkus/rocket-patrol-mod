23349d08c8a9f65e25d2a443b59515c66b2a6f2d
Drullkus <5010174+Drullkus@users.noreply.github.com>
2026-02-01 04:06:25 -0800

    Menu refinement

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -60,8 +60,9 @@ class Menu extends Phaser.Scene {
         this.mode = 0.5;
         this.modeSpeed = 0.015; // Higher values = less time to hold to enter one of the game modes
 
-        this.galaxy = this.add.tileSprite(gameWidth, 0, -gameWidth, gameHeight, 'galaxy').setOrigin(0).setAlpha(0.75);
+        this.galaxy = this.add.tileSprite(0, 0, gameWidth, gameHeight, 'galaxy').setOrigin(0).setAlpha(0.75);
         this.galaxy.tilePositionX = 200;
+        this.galaxy.flipX = true;
 
         this.noviceBar = this.add.rectangle(0, gameHeight, gameWidth, gameHeight * 0.05, 0xF3B141).setOrigin(0, 1);
         this.expertBar = this.add.rectangle(gameWidth, gameHeight, gameWidth, gameHeight * 0.05, 0x00FF00).setOrigin(1, 1);
@@ -69,7 +70,7 @@ class Menu extends Phaser.Scene {
         const menuConfig = {
             fontFamily: 'xirod',
             fontSize: '26px',
-            color: '#F3B141',
+            color: '#FFF',
             align: 'right',
             padding: {
                 top: 5,
@@ -78,13 +79,24 @@ class Menu extends Phaser.Scene {
             fixedWidth: 0
         };
         // display menu text
-        this.add.text(gameWidth * 0.5, gameHeight * 0.5 - borderUISize - borderPadding * 4, 'ROCKET TERMINAL', menuConfig).setOrigin(0.5);
+        this.add.text(gameWidth * 0.5, gameHeight * 0.5 - borderUISize - borderPadding * 4, 'ROCKET TERMINAL', {
+            ...menuConfig,
+            fontSize: '50px',
+            color: '#FFF'
+        }).setOrigin(0.5);
         this.add.text(gameWidth * 0.5, gameHeight * 0.5, 'Use ←→ arrows to move', menuConfig).setOrigin(0.5, 1);
         this.add.text(gameWidth * 0.5, gameHeight * 0.5, 'and (F) to fire', menuConfig).setOrigin(0.5, 0);
-        this.add.text(gameWidth * 0.5, gameHeight * 0.5 + borderUISize + borderPadding * 4, 'Hold ← for Novice or → for Expert', {
+
+        this.add.text(gameWidth * 0.5, gameHeight * 0.8, 'Move to select difficulty:', menuConfig).setOrigin(0.5, 0);
+
+        this.add.text(gameWidth * 0.1, gameHeight * 0.9, '← Novice', {
             ...menuConfig,
-            color: '#00FF00'
-        }).setOrigin(0.5);
+            color: '#F3B141'
+        }).setOrigin(0, 0.5);
+        this.add.text(gameWidth * 0.9, gameHeight * 0.9, 'Expert →', {
+            ...menuConfig,
+            color: '#0F0'
+        }).setOrigin(1, 0.5);
 
         keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
         keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
@@ -101,7 +113,7 @@ class Menu extends Phaser.Scene {
             this.mode = Phaser.Math.Linear(this.mode, 0.5, 0.1);
         }
 
-        this.galaxy.tilePositionX = this.mode * 400;
+        this.galaxy.tilePositionX = (1 - this.mode) * 400;
 
         this.noviceBar.scaleX = 1 - (2 * this.mode);
         this.expertBar.scaleX = 1 - (2 * (1 - this.mode));
